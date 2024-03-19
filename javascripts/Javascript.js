d2eda908dcc114f13222b414b62cc7ffe6e5fef3
JoAnn <joann.thao@live.com>
2024-03-19 14:58:31 -0500

    some JS

---
+++ javascripts/Javascript.js
@@ -0,0 +1,7 @@
+const workout= [
+    {date: 1/1/24, workout: "jumping jacks", summary: "1 minute of jumpingjacks"},
+    {date: 1/1/24, workout: "high knees", summary: "1 minute of high knees"},
+    {date: 1/3/24, workout: "planks", summary: "1 minute of planks"},
+    {date: 1/5/24, workout: "crunches", summary: "1 minute of crunches"},
+];
+
